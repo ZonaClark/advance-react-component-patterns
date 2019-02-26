243bc747350702c6c8875a411c978bca4bdbbadc
ZonaClark <zona.clark56@gmail.com>
2019-02-25 20:14:31 -0500

    use context api to provide toggle properties

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
 import React, { Component } from 'react';
 import { Switch } from './switch';
 
+const ToggleContext = React.createContext()
+
 class Toggle extends Component {
+  static On = ({children}) => (
+    <ToggleContext.Consumer>
+      {contextValue => (contextValue.on ? children : null)}
+    </ToggleContext.Consumer>
+  )
+  static Off = ({children}) => (
+    <ToggleContext.Consumer>
+      {contextValue => (contextValue.on ? null : children)}
+    </ToggleContext.Consumer>
+  )
+  static Button = props => (
+    <ToggleContext.Consumer>
+      {contextValue => (
+        <Switch 
+          on={contextValue.on} 
+          onClick={contextValue.toggle} 
+          {...props} 
+        />
+      )}
+    </ToggleContext.Consumer>
+  )
   state = {
     on: false,
   }
 
+  toggle = () =>
+    this.setState(
+      ({on}) => ({on: !on}),
+      () => this.props.onToggle(this.state.on),
+    )
+
   render() {
     return (
-      <Switch on={this.state.on} />
-    );
+      <ToggleContext.Provider
+        value={{
+          on: this.state.on,
+          toggle: this.toggle,
+        }}>
+        {this.props.children}
+      </ToggleContext.Provider>
+    )
   }
 }
 
+// root
 function Usage({
   onToggle = (...args) => console.log('onToggle', ...args),
 }) {
-  return <Toggle onToggle={onToggle} />
+  return (
+    <Toggle onToggle={onToggle}>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Off>The button is off</Toggle.Off>
+      <Toggle.Button />
+    </Toggle>
+  )
 }
 
-export default Usage;
+export default Usage
