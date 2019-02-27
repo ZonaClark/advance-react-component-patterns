3454f2c22f8e5f4f31d4911a81a06b01c2b84094
ZonaClark <zona.clark56@gmail.com>
2019-02-27 08:47:02 -0500

    use prop collections with render prop

--- src/App.js
+++ src/App.js
@@ -1,64 +1,26 @@
 import React, { Component } from 'react';
 import { Switch } from './switch';
 
-const ToggleContext = React.createContext()
-
-function ToggleConsumer(props) {
-  return (
-    <ToggleContext.Consumer>
-      {context => {
-        if (!context) {
-          throw new Error(
-            'Togggle compound components must be rendered within Toggle component.'
-          )
-        }
-        return props.children(context)
-      }}
-    </ToggleContext.Consumer>
-  )
-}
-
 class Toggle extends Component {
-  static On = ({children}) => (
-    <ToggleConsumer>
-      {({on}) => (on ? children : null)}
-    </ToggleConsumer>
-  )
-  static Off = ({children}) => (
-    <ToggleConsumer>
-      {({on}) => (on ? null : children)}
-    </ToggleConsumer>
-  )
-  static Button = props => (
-    <ToggleConsumer>
-      {({on, toggle}) => (
-        <Switch 
-          on={on} 
-          onClick={toggle} 
-          {...props} 
-        />
-      )}
-    </ToggleConsumer>
-  )
-
+  state = {on: false}
   toggle = () =>
     this.setState(
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
-  
-  state = {
-    on: false,
-    toggle: this.toggle,
+  getStateAndHelpers() {
+    return {
+      on: this.state.on,
+      toggle: this.toggle,
+      togglerProps: {
+        'aria-pressed': this.state.on,
+        onClick: this.toggle,
+      }
+    }
   }
     
   render() {
-    return (
-      <ToggleContext.Provider
-        value={this.state}>
-        {this.props.children}
-      </ToggleContext.Provider>
-    )
+    return this.props.children(this.getStateAndHelpers())
   }
 }
 
@@ -68,9 +30,15 @@ function Usage({
 }) {
   return (
     <Toggle onToggle={onToggle}>
-      <Toggle.On>The button is on</Toggle.On>
-      <Toggle.Off>The button is off</Toggle.Off>
-      <Toggle.Button />
+      {({on, togglerProps}) => (
+        <div>
+          <Switch on={on} {...togglerProps} />
+          <hr />
+          <button aria-label="custom-button" {...togglerProps}>
+            {on ? 'on' : 'off'}
+          </button>
+        </div>
+      )}
     </Toggle>
   )
 }
