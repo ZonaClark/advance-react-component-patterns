1f34029546f91b2cff21d915761dfd1e8f257893
ZonaClark <zona.clark56@gmail.com>
2019-02-27 09:08:50 -0500

    prop getters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Switch } from './switch';
 
+const callAll = (...fns) => (...args) =>
+  fns.forEach(fn => fn && fn(...args))
+
 class Toggle extends Component {
   state = {on: false}
   toggle = () =>
@@ -8,14 +11,16 @@ class Toggle extends Component {
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
+  getTogglerProps = ({onClick, ...props} = {}) => ({
+    'aria-pressed': this.state.on,
+    onClick: callAll(onClick, this.toggle),
+    ...props,
+  })
   getStateAndHelpers() {
     return {
       on: this.state.on,
       toggle: this.toggle,
-      togglerProps: {
-        'aria-pressed': this.state.on,
-        onClick: this.toggle,
-      }
+      getTogglerProps: this.getTogglerProps,
     }
   }
     
@@ -27,14 +32,21 @@ class Toggle extends Component {
 // root
 function Usage({
   onToggle = (...args) => console.log('onToggle', ...args),
+  onButtonClick = () => console.log('onButtonClick'),
 }) {
   return (
     <Toggle onToggle={onToggle}>
-      {({on, togglerProps}) => (
+      {({on, getTogglerProps}) => (
         <div>
-          <Switch on={on} {...togglerProps} />
+          <Switch {...getTogglerProps({on})} />
           <hr />
-          <button aria-label="custom-button" {...togglerProps}>
+          <button
+            {...getTogglerProps({
+              'aria-label': 'custom-button',
+              id: "custom-button-id",
+              onClick: onButtonClick,
+            })}
+          >
             {on ? 'on' : 'off'}
           </button>
         </div>
